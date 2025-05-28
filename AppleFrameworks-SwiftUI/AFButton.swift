ce7b02d79edc6a0f088b8ee266d7e3bc6d00f2c6
Lisa <imnotlostrui@aim.com>
2025-05-28 11:30:31 -0400

    first commit

---
+++ AppleFrameworks-SwiftUI/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Lisa J on 5/28/25.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
---
+++ AppleFrameworks-SwiftUI/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Lisa J on 5/27/25.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
